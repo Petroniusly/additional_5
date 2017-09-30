aed31ab9af28620de66dcabe26c736561e34ba2f
Petroniusly <blinkovpag@gmail.com>
2017-09-30 10:13:21 +0300

    Clean for production

--- src/index.js
+++ src/index.js
@@ -1,43 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   
   var array = str.split('');
-/* 
-  First bad idea for loop the look only number open/closed brackets without positionin string
-  for (var i = 0; i < bracketsConfig.length; i++) {
-    var open = bracketsConfig[i][0];
-    var close = bracketsConfig[i][1];
-    var count = 0;
-    var countArr = [];
-
-    for (var j = 0; j < array.length; j++) {
-      if (array[j] == open) {
-        count++;
-        countArr.push( ' + ');
-      }
-      if (array[j] == close) {
-        count--;
-        countArr.push('- ');}
-      if (count < 0) {
-        //return count + ' : ' + countArr.join('');
-        return false;
-      }
-    }
-
-    if (count > 0) {
-      //return count + ' : ' + countArr.join('');
-      return false;
-    }
-
-    //return count + ' : ' + countArr.join('');
-  }
-  
-  //return count + ' : ' + countArr.join('');
-  return true;
-
-*/
-
   var stack = [];
-
    
   for (var j = 0; j < array.length; j++) {
     for (var i = 0; i < bracketsConfig.length; i++) {
@@ -50,7 +14,6 @@ module.exports = function check(str, bracketsConfig) {
           stack.push(i);
           break;
         } else {
-          //return 'first false : ' + stack;
           return false;
         }
       } else if (array[j] == bracketsConfig[i][0]) { //item in array equal some open bracket, add to stack type of bracket (remember number i)
@@ -59,6 +22,5 @@ module.exports = function check(str, bracketsConfig) {
       } 
     }
   }
-  //return 'finish, stack is ' + stack;
   return stack.length == 0 ? true : false;
 }
